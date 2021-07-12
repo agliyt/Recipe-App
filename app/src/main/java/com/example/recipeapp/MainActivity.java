1b57249b6d7324abb61044a0d7cce5f3d59a5125
agliyt <angiel@andrew.cmu.edu>
2021-07-12 12:48:37 -0400

    made fragments for all the different views

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.recipeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.recipeapp.fragments.ComposeFragment;
+import com.example.recipeapp.fragments.FavoritesFragment;
+import com.example.recipeapp.fragments.IngredientsFragment;
+import com.example.recipeapp.fragments.RecipesFragment;
+import com.example.recipeapp.fragments.ShoppingListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        Menu menu = bottomNavigationView.getMenu();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                menu.findItem(R.id.ingredientsTab).setIcon(R.drawable.ic_round_view_list_24);
+                menu.findItem(R.id.recipesTab).setIcon(R.drawable.ic_outline_fastfood_24);
+                menu.findItem(R.id.composeTab).setIcon(R.drawable.ic_outline_create_24);
+                menu.findItem(R.id.shoppingListTab).setIcon(R.drawable.ic_outline_shopping_cart_24);
+                menu.findItem(R.id.favoritesTab).setIcon(R.drawable.ic_round_star_outline_24);
+
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.ingredientsTab:
+                        fragment = new IngredientsFragment();
+                        break;
+                    case R.id.recipesTab:
+                        item.setIcon(R.drawable.ic_baseline_fastfood_24);
+                        fragment = new RecipesFragment();
+                        break;
+                    case R.id.composeTab:
+                        item.setIcon(R.drawable.ic_round_create_24);
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.shoppingListTab:
+                        item.setIcon(R.drawable.ic_baseline_shopping_cart_24);
+                        fragment = new ShoppingListFragment();
+                        break;
+                    case R.id.favoritesTab:
+                        item.setIcon(R.drawable.ic_outline_star_24);
+                        fragment = new FavoritesFragment();
+                        break;
+                    default:
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // set default
+        bottomNavigationView.setSelectedItemId(R.id.ingredientsTab);
     }
 }
